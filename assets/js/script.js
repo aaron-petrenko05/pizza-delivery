c13e04e727a15cac3afb542fd5fc892349675119
aaron-petrenko05 <44132538+aaron-petrenko05@users.noreply.github.com>
2018-10-15 14:02:42 +0800

    updated assets files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
+
 $(document).ready(function() {
+	$("#showHome").addClass("active");
+	$("#pizza-order").hide();
+	$("#location").hide();
+	$("#home").show();
+
 	$("#showHome").click(function() {
 		$("#pizza-order").hide();
 		$("#location").hide();
@@ -15,5 +21,14 @@ $(document).ready(function() {
 		$("#home").hide();
 		$("#location").show();
 		$("#pizza-order").hide();
+	});
+
+	$(".navbar-nav li a").click(function() {
+	  // remove classes from all
+	  $(".navbar-nav li a").removeClass("active");
+	  // add class to the one we clicked
+	  $(this).addClass("active");
+	  // stop the page from jumping to the top
+	  return false;
 	});
 });
